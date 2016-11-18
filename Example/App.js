9c69ed10ddafdb2bf6e309c256491b3492125756
Thibault Lenclos <tlenclos@jolicode.com>
2016-11-18 18:19:29 +0100

    Add multiple stream in example app

--- Example/App.js
+++ Example/App.js
@@ -4,19 +4,67 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    ListView,
+    Platform,
+    TouchableOpacity
 } from 'react-native';
-import { RNReactNativeAudioStreaming, Player } from 'react-native-audio-streaming';
-
-const url = 'http://www.stephaniequinn.com/Music/Canon.mp3';
+import { ReactNativeAudioStreaming, Player } from 'react-native-audio-streaming';
 
 export default class App extends Component {
+    constructor() {
+        super();
+        this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.urls = [
+            {
+                name: 'Shoutcast stream',
+                url: 'http://lacavewebradio.chickenkiller.com:8000/stream.mp3'
+            },
+            {
+                name: 'M4A stream',
+                url: 'http://web.ist.utl.pt/antonio.afonso/www.aadsm.net/libraries/id3/music/02_Viandanze.m4a'
+            },
+            {
+                name: 'MP3 stream with ID3 meta data',
+                url: 'http://web.ist.utl.pt/antonio.afonso/www.aadsm.net/libraries/id3/music/Bruno_Walter_-_01_-_Beethoven_Symphony_No_1_Menuetto.mp3'
+            },
+            {
+                name: 'MP3 stream',
+                url: 'http://www.stephaniequinn.com/Music/Canon.mp3'
+            }
+        ];
+
+        this.state = {
+            dataSource: this.ds.cloneWithRows(this.urls),
+            selectedSource: this.urls[0].url
+        };
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Playing stream {url}</Text>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) =>
+                        <TouchableOpacity onPress={() => {
+                            this.setState({selectedSource: rowData.url, dataSource: this.ds.cloneWithRows(this.urls)});
+                            ReactNativeAudioStreaming.play(rowData.url, {});
+                        }}>
+                            <View style={StyleSheet.flatten([
+                                styles.row,
+                                {backgroundColor: rowData.url == this.state.selectedSource ? '#3fb5ff' : 'white'}
+                            ])}>
+                                <Text style={styles.icon}>▸</Text>
+                                <View style={styles.column}>
+                                    <Text style={styles.name}>{rowData.name}</Text>
+                                    <Text style={styles.url}>{rowData.url}</Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    }
+                />
 
-                <Player url={url} />
+                <Player url={this.state.selectedSource} />
             </View>
         );
     }
@@ -28,15 +76,27 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+        paddingTop: Platform.OS === 'ios' ? 30 : 0
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 5,
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    column: {
+        flexDirection: 'column'
     },
+    icon: {
+        fontSize: 26,
+        width: 30,
+        textAlign: 'center'
+    },
+    name: {
+        color: '#000'
+    },
+    url: {
+        color: '#CCC'
+    }
 });
